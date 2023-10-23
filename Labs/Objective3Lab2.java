709c9630a33e954945e26617ddd2fb9572319e6e
Ashley Mascarenas <ashleymascarenas@yahoo.com>
2023-10-23 12:47:19 -0600

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,14 @@
+public class Objective3Lab2 {
+	public static void main (String[] args) {
+		double side1, side2, hypotenuse;  // declare variables side1, side2, hypotenuse as double
+		side1 = 8;			// initialize side1 variable			
+		side2 = 19;			// initialize side2 variable
+		hypotenuse = Math.sqrt((side1 * side1) + (side2 * side2));  // initialize hypotenuse variable as equation of right triangle a^2+b^2=c^2
+		System.out.println("The hypotenuse of a trinagle with sides " + side1 + " and " + side2 + " is " + hypotenuse);		// print out answer
+
+
+
+
+
+	}
+}
