68b9cacc9c54d0de35f427c8255a1072625cd314
Ashley Mascarenas <ashleymascarenas@yahoo.com>
2023-10-23 12:17:09 -0600

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 34;
+    num2 = 3;
+    num3 = 101;
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
