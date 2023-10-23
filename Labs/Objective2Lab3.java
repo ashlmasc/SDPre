e086eb84ffe4227bb61f45e6d2e55c3625ee29e7
Ashley Mascarenas <ashleymascarenas@yahoo.com>
2023-10-23 11:42:29 -0600

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int yearBorn = 1989;
+    String monthBorn = "March";
+    boolean weekend = false;
+    System.out.println(yearBorn);
+    System.out.println(monthBorn);
+    System.out.println(weekend);
+  }
+}
