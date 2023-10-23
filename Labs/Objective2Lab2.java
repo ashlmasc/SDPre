ef5706ca9e699b1dbcca6a38a66907f1bba1c3ce
Ashley Mascarenas <ashleymascarenas@yahoo.com>
2023-10-23 11:27:58 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ashley Mascarenas";
+    int age = 34;
+    String state = "New Mexico";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
