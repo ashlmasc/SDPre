42cf04bd31e94a0d493a1a62b6f52bd338fd89a3
Ashley Mascarenas <ashleymascarenas@yahoo.com>
2023-11-04 14:04:34 -0600

    complete objective7lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,10 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+  int counter = 0;    // Declares and initializes integer variable counter to equal 0
+
+    while(counter <= 20) {    // Program checks the condition counter <= 20; if condition is met program prints out counter and then adds 1 to counter; loop continues running until condition is false and then program exits the loop
+       System.out.println(counter);
+      counter = counter + 1;    // Equation that adds 1 to counter and sets counter to equal new number
+    }
+  }
+}
