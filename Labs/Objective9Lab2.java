8b6f4737610d06936bf8e7553e08456f989cb02a
Ashley Mascarenas <ashleymascarenas@yahoo.com>
2023-11-04 17:04:59 -0600

    complete objective9lab2

---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,11 @@
+public class Objective9Lab2 {
+  public static void main(String[] args) { // Main method
+    System.out.println("In main");    // Prints out message In main
+    printMessage();   // Calls printMessage method
+    System.out.println("In main");    // Prints out message In main
+  }
+
+  public static void printMessage() {   // Method printMessage that prints out message In method
+    System.out.println("In method");
+  }
+}
