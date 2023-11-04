5f584bd41648a7af827c158b91a9c36e9b9856d0
Ashley Mascarenas <ashleymascarenas@yahoo.com>
2023-11-04 16:53:56 -0600

    complete objective9lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,10 @@
+public class Objective9Lab1 {
+  public static void main(String[] args) { // Main method that calls method printName
+    printName(); 
+    
+  }
+
+  public static void printName() {    // Method printName the prints out message Hello Doug
+    System.out.println("Hello Doug");
+  }
+}
