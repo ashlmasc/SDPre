5b388ec16a14f4704eb66ecd4c28f6b4569b8dd0
Ashley Mascarenas <ashleymascarenas@yahoo.com>
2023-11-04 16:01:03 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    
+    for (int i=1; i<=10; i+=1){   // for loop that declares and initializes the integer i to 1, excutes the loop while i is less than or equal to 10, and increases i by 1 increment each time the loop executes
+      System.out.println(i);      // Prints out i
+    }
+
+    for (int i=10; i>=1; i-=1){   // for loop that declares and initializes the integer i to 10, executes the loop while i is greater than or equal to 1, and decreases i by 1 increment each time the loop executes
+      System.out.println(i);      // Prints out i
+    }
+  }
+}
